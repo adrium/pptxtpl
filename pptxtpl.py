1920a5f58ccddbc859d155158ef3a9010ec87719
Adrian <adrian.dev@kousz.ch>
2023-11-12 08:30:10 +0100

    Use regex for replacements and use I as delimiter

--- pptxtpl.py
+++ pptxtpl.py
@@ -1,21 +1,38 @@
 from fnmatch import fnmatch
 from io import BytesIO
+from re import compile as regex
 from zipfile import ZipFile, ZIP_DEFLATED
 import json
 
-def main(infn: str, outfn: str, vars: dict, numerics: dict):
-	replacements = getreplacements(vars, numerics)
+def main(infn: str, numerics: dict, vars: dict, outfn: str):
+
+	def replacer(param, empty):
+		result = empty
+		if param in numerics:
+			param = numerics[param]
+		if param in vars:
+			result = vars[param]
+		if (isinstance(result, str)):
+			result = result.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
+		return result
+
+	replacers = [
+		{ 're': regex('I([A-Z][A-Za-z0-9]+)I'), 'sub': lambda m: replacer(m.group(1), ' ') },
+		{ 're': regex('>(([A-Z][a-z0-9]+){2,})<'), 'sub': lambda m: f">{replacer(m.group(1), m.group(1))}<" },
+		{ 're': regex('>9([0-9]{3,4})9<'), 'sub': lambda m: f">{replacer(m.group(1), '0')}<" },
+	]
 
 	with open(outfn, 'wb') as outfile:
-		processzip(infn, outfile, replacements)
+		processzip(infn, outfile, replacers)
 
-def processzip(infile, outfile, replacements: dict):
+def processzip(infile, outfile, replacers: list):
+
 	with ZipFile(infile, 'r') as inpptx, ZipFile(outfile, 'w', ZIP_DEFLATED) as outpptx:
 		for name in inpptx.namelist():
 			with inpptx.open(name) as memberfile:
 				if fnmatch(name, 'ppt/embeddings/*'):
 					with BytesIO() as embed:
-						processzip(memberfile, embed, replacements)
+						processzip(memberfile, embed, replacers)
 						embed.seek(0)
 						outpptx.writestr(name, embed.read())
 					continue
@@ -24,44 +41,18 @@ def processzip(infile, outfile, replacements: dict):
 
 				if fnmatch(name, '*.xml'):
 					xml = memberdata.decode()
-					for x, y in replacements.items():
-						xml = xml.replace(x, y)
+					for re in replacers:
+						xml = re['re'].sub(re['sub'], xml)
 					memberdata = xml.encode()
 
 				outpptx.writestr(name, memberdata)
 
-def getreplacements(vars: dict, numerics: dict):
-	result = {}
-
-	for k, n in numerics.items():
-		result[f'>9{n}9</'] = '>0</'
-
-	for k, v in vars.items():
-		if (isinstance(v, str)):
-			v = v.replace('&', '&amp;')
-
-		result[f'{{{k}}}'] = f'{v}'
-		result[f'>{k}</'] = f'>{v}</'
-
-		if k in numerics:
-			n = numerics[k]
-			result[f'>9{n}9</'] = f'>{v}</'
-
-	result['{</'] = '</'
-	result['>}'] = '>'
-
-	return result
-
 def loadJson(file: str) -> dict:
+
 	with open(file) as f:
 		return json.load(f)
 
 if __name__ == '__main__':
+
 	import sys
-	data = {}
-	try:
-		data = loadJson(sys.argv[3])
-	except:
-		print('failed to load ' + sys.argv[3])
-	if data != {}:
-		main(sys.argv[1], sys.argv[2], data, loadJson(sys.argv[4]))
+	main(sys.argv[1], loadJson(sys.argv[2]), loadJson(sys.argv[3]), sys.argv[4])
